f9b44dbe02d487880edaeda4936ed178580f2555
andraaspar <andraaspar@gmail.com>
2015-08-07 16:37:35 +0200

    Made stdout the default for output.

--- src/ts/main/Main.ts
+++ src/ts/main/Main.ts
@@ -7,6 +7,7 @@
 
 /// <reference path='../../../lib/pml/HtmlStringer.ts'/>
 /// <reference path='../../../lib/pml/Linter.ts'/>
+/// <reference path='../../../lib/pml/Message.ts'/>
 /// <reference path='../../../lib/pml/Parser.ts'/>
 /// <reference path='../../../lib/pml/Tidier.ts'/>
 
@@ -21,6 +22,10 @@ module pml_cli {
 
 		private static instance = new Main();
 		
+		private messages: pml.Message[] = [];
+		private argsRead: boolean = false;
+		private outputToStdOut: boolean;
+		
 		private args: string[];
 		private command: string;
 		private inFileContents: string;
@@ -30,7 +35,7 @@ module pml_cli {
 		private indentChar: string;
 
 		constructor() {
-			this.log(chalk.bold('\nPair Markup Language processor.') + '   Version: ___PACKAGE_VERSION___\n');
+			this.log(chalk.bold('Pair Markup Language processor.') + '   Version: ___PACKAGE_VERSION___\n');
 			
 			this.args = process.argv.slice(2);
 			//illa.Log.info(chalk.cyan('Arguments: ') + args.join(', '));
@@ -39,9 +44,16 @@ module pml_cli {
 			this.inFilePath = this.args.pop();
 			this.parseOptions();
 			
+			this.argsRead = true;
+			this.outputToStdOut = !this.outFilePath;
+			this.flushLogBuffer();
+			
 			switch (this.command) {
 				case 'l':
 				case 'lint':
+					this.outputToStdOut = false;
+					this.flushLogBuffer();
+					
 					this.loadInFile();
 					this.log(chalk.bold('Linting ' + this.inFilePath + ' ...'));
 					
@@ -54,8 +66,9 @@ module pml_cli {
 					this.loadInFile();
 					this.log(chalk.bold('Tidying ' + this.inFilePath + ' ...'));
 					
-					if (!this.outFilePath) this.isOverwriteAllowed = true;
-					this.checkOutFileExists();
+					if (this.outFilePath) {
+						this.checkOutFileExists();
+					}
 					
 					var tidier = new pml.Tidier();
 					if (illa.isString(this.indentChar)) {
@@ -63,7 +76,8 @@ module pml_cli {
 					}
 					
 					var outFileContents = tidier.tidy(this.inFileContents);
-					this.writeOutFile(this.outFilePath || this.inFilePath, outFileContents);
+					
+					this.writeOutput(this.outFilePath, outFileContents);
 					break;
 				case 'h':
 				case 'html':
@@ -71,8 +85,8 @@ module pml_cli {
 					this.log(chalk.bold('Converting ' + this.inFilePath + ' to HTML ...'));
 					
 					if (!this.outFilePath) {
-						this.warn('No output file specified.');
-						process.exit(1);
+						this.error('No output file specified.');
+						this.fail(1);
 					}
 					this.checkOutFileExists();
 					
@@ -84,10 +98,13 @@ module pml_cli {
 					}
 					
 					var outFileContents = htmlStringer.stringify(parser.parse(this.inFileContents));
-					this.writeOutFile(this.outFilePath, outFileContents);
+					this.writeOutput(this.outFilePath, outFileContents);
 					break;
 				case '?':
 				case 'help':
+					this.outputToStdOut = false;
+					this.flushLogBuffer();
+					
 					this.log('Usage: ' + chalk.bold('pml [command] [options...] [input file]'));
 					this.log(chalk.bold('\nCommands:') + `
 ?, help ................... Displays this help.
@@ -96,20 +113,22 @@ t, tidy ................... Tidies the input file.
 l, lint ................... Lints the input file.
 `);
 					this.log(chalk.bold('\nOptions for HTML mode:') + `
--o, --out [output file] ... Required. Specifies output file path.
+-o, --out [output file] ... Specifies output file path. If not specified,
+                            defaults to stdout.
 -ow, --overwrite .......... Overwrite existing output file(s).
---indentChar [string] ..... Specifies the character(s) used for indentation.
+--indent-char [string] .... Specifies the character(s) used for indentation.
 `);
 					this.log(chalk.bold('\nOptions for Tidy mode:') + `
 -o, --out [output file] ... Specifies output file path. If not specified,
-                            defaults to the input file.
+                            defaults to stdout.
 -ow, --overwrite .......... Overwrite existing output file(s).
---indentChar [string] ..... Specifies the character(s) used for indentation.
+--indent-char [string] .... Specifies the character(s) used for indentation.
 `);
 					break;
 				default:
-					this.warn('Command not recognized.');
-					this.log('Please specify a valid command, or issue \'pml help\' for more information.');
+					this.error('Command not recognized.');
+					this.errorInfo('Please specify a valid command, or issue \'pml help\' for more information.');
+					this.fail(1);
 			}
 		}
 		
@@ -121,32 +140,32 @@ l, lint ................... Lints the input file.
 					case '--out':
 						this.outFilePath = this.args[++i];
 						if (!this.outFilePath) {
-							this.warn(arg + ' is missing the file parameter.');
-							process.exit(1);
+							this.error(arg + ' is missing the file parameter.');
+							this.fail(1);
 						}
 						break;
 					case '-ow':
 					case '--overwrite':
 						this.isOverwriteAllowed = true;
 						break;
-					case '--indentChar':
+					case '--indent-char':
 						this.indentChar = this.args[++i];
 						if (!illa.isString(this.indentChar)) {
-							this.warn(arg + ' is missing the string parameter.');
-							process.exit(1);
+							this.error(arg + ' is missing the string parameter.');
+							this.fail(1);
 						}
 						break;
 					default:
-						this.warn('Invalid option: ' + arg);
-						process.exit(1);
+						this.error('Invalid option: ' + arg);
+						this.fail(1);
 				}
 			}
 		}
 		
 		loadInFile(): void {
 			if (!fs.existsSync(this.inFilePath)) {
-				this.warn('Input file not found.');
-				process.exit(1);
+				this.error('Input file not found.');
+				this.fail(1);
 			}
 			
 			this.inFileContents = fs.readFileSync(this.inFilePath, 'utf8');
@@ -154,27 +173,68 @@ l, lint ................... Lints the input file.
 		
 		checkOutFileExists(): void {
 			if (!this.isOverwriteAllowed && fs.existsSync(this.outFilePath)) {
-				this.warn('Output file already exists.');
-				this.log('Use \'--overwrite\' to ignore.');
-				process.exit(1);
+				this.error('Output file already exists.');
+				this.errorInfo('Use \'--overwrite\' to ignore.');
+				this.fail(1);
 			}
 		}
 		
-		writeOutFile(outFilePath: string, outFileContents: string): void {
-			fs.writeFileSync(outFilePath, outFileContents);
-			this.success('Output written to ' + outFilePath + '.');
+		writeOutput(outFilePath: string, outFileContents: string): void {
+			if (outFilePath) {
+				fs.writeFileSync(outFilePath, outFileContents);
+				this.success('Output written to ' + outFilePath + '.');
+			} else {
+				process.stdout.write(outFileContents);
+			}
 		}
 		
-		warn(...args): void {
-			illa.Log.warn(chalk.yellow.bold(args.join(' ')));
+		error(...args): void {
+			this.errorInfo(chalk.yellow.bold(args.join(' ')));
+		}
+		
+		errorInfo(...args): void {
+			var message = args.join(' ') + '\n';
+			if (this.argsRead) {
+				process.stderr.write(message);
+			} else {
+				this.messages.push(new pml.Message(pml.MessageKind.ERROR, 0, 0, message));
+			}
 		}
 		
 		success(...args): void {
-			illa.Log.info(chalk.green(args.join(' ')));
+			this.log(chalk.green(args.join(' ')));
 		}
 		
 		log(...args): void {
-			illa.Log.log(args.join(' '));
+			var message = args.join(' ') + '\n';
+			if (this.argsRead && !this.outputToStdOut) {
+				process.stdout.write(message);
+			} else {
+				this.messages.push(new pml.Message(pml.MessageKind.INFO, 0, 0, message));
+			}
+		}
+		
+		flushLogBuffer(): void {
+			for (var i = 0; i < this.messages.length; i++) {
+				var message = this.messages[i];
+				switch (message.kind) {
+					case pml.MessageKind.ERROR:
+					case pml.MessageKind.WARNING:
+						process.stderr.write(message.message);
+						this.messages.splice(i, 1);
+						i--;
+						break;
+					default:
+						if (!this.outputToStdOut) {
+							process.stdout.write(message.message);
+						}
+				}
+			}
+		}
+		
+		fail(code: number): void {
+			this.flushLogBuffer();
+			process.exit(code);
 		}
 	}
 }
